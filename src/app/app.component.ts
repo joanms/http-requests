b4cb9966cde401ccdb47e50ebcaed7a6d7cd5512
Joan Sweeney <joanms@eircom.net>
2019-11-15 17:53:11 +0000

    Add post model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/Operators';
 
+import { Post } from './post.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,10 +18,10 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
-      .post(
+      .post<{ name: string }>(
         'https://udemy-angular-http-modul-e6d0b.firebaseio.com/posts.json',
         postData
       )
@@ -39,10 +41,10 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http
-    .get('https://udemy-angular-http-modul-e6d0b.firebaseio.com/posts.json')
+    .get<{ [key: string]: Post }>('https://udemy-angular-http-modul-e6d0b.firebaseio.com/posts.json')
     .pipe(
       map(responseData => {
-        const postsArray = [];
+        const postsArray: Post[] = [];
         for (const key in responseData) {
           if (responseData.hasOwnProperty(key)) {
             postsArray.push({ ...responseData[key], id: key });
