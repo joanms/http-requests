a4a7397da563e8016c999f9de3745741a5a6a8f2
Joan Sweeney <joanms@eircom.net>
2019-11-19 10:52:55 +0000

    Move code for fetching posts from component to service

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -22,20 +22,20 @@ export class PostsService {
     }
 
     fetchPosts() {
-        this.http
-        .get<{ [key: string]: Post }>('https://udemy-angular-http-modul-e6d0b.firebaseio.com/posts.json')
-        .pipe(
-          map(responseData => {
-            const postsArray: Post[] = [];
-            for (const key in responseData) {
-              if (responseData.hasOwnProperty(key)) {
-                postsArray.push({ ...responseData[key], id: key });
-              }
-            }
-            return postsArray;
-          })
-        )
-        .subscribe(posts => {
-        });
+        return this.http
+            .get<{ [key: string]: Post }>(
+                'https://udemy-angular-http-modul-e6d0b.firebaseio.com/posts.json'
+                )
+            .pipe(
+                map(responseData => {
+                    const postsArray: Post[] = [];
+                    for (const key in responseData) {
+                        if (responseData.hasOwnProperty(key)) {
+                            postsArray.push({ ...responseData[key], id: key });
+                        }
+                    }
+                    return postsArray;
+                })
+            );
     }
 }
