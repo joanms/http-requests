b4be5ab8bee8a3b039ee53f09deea766e639365f
Joan Sweeney <joanms@eircom.net>
2019-11-19 10:30:24 +0000

    Add a service for Http requests

---
+++ src/app/posts.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/Operators';
+
+import { Post } from './post.model';
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+
+    constructor(private http: HttpClient) {}
+
+    createAndStorePost(title: string, content: string) {
+        const postData: Post = {title: title, content: content};
+        this.http
+        .post<{ name: string }>(
+          'https://udemy-angular-http-modul-e6d0b.firebaseio.com/posts.json',
+          postData
+        )
+        .subscribe(responseData => {
+          console.log(responseData);
+        });
+    }
+
+    fetchPosts() {
+        this.http
+        .get<{ [key: string]: Post }>('https://udemy-angular-http-modul-e6d0b.firebaseio.com/posts.json')
+        .pipe(
+          map(responseData => {
+            const postsArray: Post[] = [];
+            for (const key in responseData) {
+              if (responseData.hasOwnProperty(key)) {
+                postsArray.push({ ...responseData[key], id: key });
+              }
+            }
+            return postsArray;
+          })
+        )
+        .subscribe(posts => {
+        });
+    }
+}
