abdfd5c795fa7357b3fe64f664b6691fdc402458
Joan Sweeney <joanms@eircom.net>
2019-11-19 11:12:09 +0000

    Enable deletion of posts

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -38,4 +38,8 @@ export class PostsService {
                 })
             );
     }
+
+    deletePosts() {
+        return this.http.delete('https://udemy-angular-http-modul-e6d0b.firebaseio.com/posts.json');
+    }
 }
